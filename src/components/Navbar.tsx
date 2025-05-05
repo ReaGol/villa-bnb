538b1fa22ec5ab982f6374d467276f5b919d730c
ReaGol <reutgoldin@gmail.com>
2025-05-05 17:08:40 +0300

    Initial project setup: Tailwind, RTL layout, Navbar, and base pages
    - Tailwind CSS setup and working
    - Navbar integrated into layout
    - Created pages: Home, Gallery, Contact, About, Booking, Admin
    - Home page basic design with hero image and CTA

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className='flex justify-center space-x-4 bg-gray-100 p-4 text-lg'>
+      <Link href='/'>בית</Link>
+      <Link href='/gallery'>גלריה</Link>
+      <Link href='/contact'>צור קשר</Link>
+      <Link href='/about'>אודות</Link>
+      <Link href='/booking'>הזמנה</Link>
+      <Link href='/admin'>ניהול</Link>
+    </nav>
+  );
+}
