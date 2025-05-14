e3ccbb3505aa8862965fbb973f15b04eb6a17617
ReaGol <reutgoldin@gmail.com>
2025-05-14 19:06:45 +0300

    feat: Setup admin registration functionality
    - Installed dependencies: mongoose, jwt, bcrypt
    - Connected to the database
    - Created Admin model
    - Implemented Admin registration route with email uniqueness check and password hashing

---
+++ src/models/Admin.ts
@@ -0,0 +1,18 @@
+import { Schema, model, models } from "mongoose";
+
+const adminSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Admin = models.Admin || model("Admin", adminSchema);
